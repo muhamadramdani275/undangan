9f8bec4e85ffebd7684e924b45f4a4b58c04d469
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-03-11 09:37:13 +0700

    Add nama tamu properties get from segment

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,10 +4,11 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-	public function index()
+	public function index($slug = '')
 	{
 		$data = [
 			'title'		=> "Ma'ruf Robani & Poho aran | Wedding Invitation",
+			'namaTamu'	=> str_replace('_', ' ', $slug),
 		];
 
 		return view('home', $data);
